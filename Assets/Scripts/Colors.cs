7e52047435a8b7b9f72f3a47ad9b73b80c09d1a2
NEUMONT\rmaple <rmaple@neumont.edu>
2017-09-27 16:23:55 -0600

    update colors (dropdown bit selector)

--- Assets/Scripts/Colors.cs
+++ Assets/Scripts/Colors.cs
@@ -7,15 +7,23 @@ using UnityEngine.UI;
 public class Colors : MonoBehaviour
 {
     [SerializeField] Image m_rgb = null;
+
     [SerializeField] Image m_red = null;
-    [SerializeField] Image m_green = null;
-    [SerializeField] Image m_blue = null;
-
     [SerializeField] TMPro.TextMeshProUGUI m_redText = null;
-    [SerializeField] TMPro.TextMeshProUGUI m_greenText = null;
-    [SerializeField] TMPro.TextMeshProUGUI m_blueText = null;
-    [SerializeField] TMPro.TextMeshProUGUI m_alphaText = null;
+    [SerializeField] Slider m_redSlider = null;
 
+    [SerializeField] Image m_green = null;
+    [SerializeField] TMPro.TextMeshProUGUI m_greenText = null;
+    [SerializeField] Slider m_greenSlider = null;
+
+    [SerializeField] Image m_blue = null;
+    [SerializeField] TMPro.TextMeshProUGUI m_blueText = null;
+    [SerializeField] Slider m_blueSlider = null;
+    
+    [SerializeField] TMPro.TextMeshProUGUI m_alphaText = null;
+    [SerializeField] Slider m_alphaSlider = null;
+
+    [SerializeField] TMP_Dropdown m_dropdown = null;
     [SerializeField] TMPro.TextMeshProUGUI m_descriptionText = null;
 
     public struct colorDefinition
@@ -39,7 +47,7 @@ public class Colors : MonoBehaviour
         }
     }
 
-    string[] bitDepth = { "8-Bit", "16-Bit(4)", "16-Bit(1)", "32-Bit" };
+    string[] bitDepth = { "8-Bit", "16-Bit(1)", "16-Bit(4)", "24-Bit", "32-Bit" };
     int bitDepthIndex = 0;
     Dictionary<string, colorDefinition> m_colorDefinitions = new Dictionary<string, colorDefinition>();
     colorDefinition m_colorDefinition;
@@ -47,11 +55,39 @@ public class Colors : MonoBehaviour
     private void Start()
     {
         m_colorDefinitions["8-Bit"] = new colorDefinition(3, 3, 2, 0, 256, "3|3|2|0");
-        m_colorDefinitions["16-Bit(4)"] = new colorDefinition(4, 4, 4, 4, 4096, "4|4|4|4");
         m_colorDefinitions["16-Bit(1)"] = new colorDefinition(5, 5, 5, 1, 32768, "5|5|5|1");
+        m_colorDefinitions["16-Bit(4)"] = new colorDefinition(4, 4, 4, 4, 4096, "4|4|4|4");
+        m_colorDefinitions["24-Bit"] = new colorDefinition(8, 8, 8, 0, 16777216, "8|8|8|0");
         m_colorDefinitions["32-Bit"] = new colorDefinition(8, 8, 8, 8, 16777216, "8|8|8|8");
 
         m_colorDefinition = m_colorDefinitions[bitDepth[bitDepthIndex]];
+
+        m_redSlider.value = 1.0f;
+        m_greenSlider.value = 1.0f;
+        m_blueSlider.value = 1.0f;
+        m_alphaSlider.value = 1.0f;
+
+        Reset();
+    }
+
+    private void Reset()
+    {
+        // reset the sliders
+        float value = m_redSlider.value;
+        m_redSlider.value = -value;
+        m_redSlider.value = value;
+
+        value = m_greenSlider.value;
+        m_greenSlider.value = -value;
+        m_greenSlider.value = value;
+
+        value = m_blueSlider.value;
+        m_blueSlider.value = -value;
+        m_blueSlider.value = value;
+
+        value = m_alphaSlider.value;
+        m_alphaSlider.value = -value;
+        m_alphaSlider.value = value;
     }
 
     void Update()
@@ -79,7 +115,7 @@ public class Colors : MonoBehaviour
 
         string scalarString = scalar.ToString("F2");
         string integerString = integer.ToString();
-        m_redText.text = "RED: " + scalarString + "/" + integerString;
+        m_redText.text = "RED (" + scalarString + "): " + integerString;
     }
 
     public void GreenSlider(Slider slider)
@@ -94,7 +130,7 @@ public class Colors : MonoBehaviour
 
         string scalarString = scalar.ToString("F2");
         string integerString = integer.ToString();
-        m_greenText.text = "GREEN: " + scalarString + "/" + integerString;
+        m_greenText.text = "GREEN (" + scalarString + "): " + integerString;
     }
 
     public void BlueSlider(Slider slider)
@@ -109,7 +145,7 @@ public class Colors : MonoBehaviour
 
         string scalarString = scalar.ToString("F2");
         string integerString = integer.ToString();
-        m_blueText.text = "BLUE: " + scalarString + "/" + integerString;
+        m_blueText.text = "BLUE (" + scalarString + "): " + integerString;
     }
 
     public void AlphaSlider(Slider slider)
@@ -124,6 +160,12 @@ public class Colors : MonoBehaviour
 
         string scalarString = scalar.ToString("F2");
         string integerString = integer.ToString();
-        m_alphaText.text = "ALPHA: " + scalarString + "/" + integerString;
+        m_alphaText.text = "ALPHA (" + scalarString + "): " + integerString;
+    }
+
+    public void BitDepthSelector(TMP_Dropdown dropdown)
+    {
+        m_colorDefinition = m_colorDefinitions[bitDepth[dropdown.value]];
+        Reset();
     }
 }
